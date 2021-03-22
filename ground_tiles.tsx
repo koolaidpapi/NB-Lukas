6691ea4de5500792121156f1b0b8e059f0a1cf8f
koolaidpapi <cagatay.bahar@grabarzundpartner.de>
2021-03-22 14:59:57 +0100

    New Stuff, flattened map

---
+++ ground_tiles.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="ground_tiles" tilewidth="32" tileheight="32" tilecount="441" columns="21">
+ <image source="../../../Desktop/ground_tiles.png" width="672" height="701"/>
+ <tile id="67">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
